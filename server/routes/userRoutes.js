89f4b643561daf3d577e30a2defd61187f988a49
Kanika Singhal <kanikasin216@gmail.com>
2025-08-01 01:33:16 +0530

    post

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { handleOmnidimWebhook } = require('../controllers/userController');
 
-router.get('/omnidim-data', handleOmnidimWebhook);
+router.post('/omnidim-data', handleOmnidimWebhook);
 
 module.exports = router;
